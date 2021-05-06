900da86ba86b5f4f1c65f2e008611ec2f06f1a39
Guilherme Ventura de Souza <venturalp@gmail.com>
2021-05-05 22:49:58 -0300

    chore: assets and theme

--- src/Config/Config.theme.js
+++ src/Config/Config.theme.js
@@ -1,4 +1,10 @@
 export const mainTheme = {
-  mainColor: '#0d3692',
-  pxToRem: px => `${px / 16}rem`,
+  pxToRem: (px, base = 16) => `${px / base}rem`,
+  background: '#F7F7F7',
+  mainColor: '#DE4B4B',
+  text: '#3C3C3C',
+  textContrast: '#ffffff',
+  textShadow: '0px 1px 2px #000000B3',
+  disabledColor: '#C9C9C9',
+  error: '#EB5757',
 }
